55fb433319a9a24c869b754fd06e434fdbc4ea1b
FaanKuun <afanafitro@gmail.com>
2023-07-07 14:03:28 +0700

    coba

---
+++ database/migrations/2023_07_06_030209_create_peminjaman_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeminjamanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peminjaman', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('judul_buku');
+            $table->date('tgl_peminjaman');
+            $table->date('tgl_kembali');
+            $table->Timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peminjaman');
+    }
+}
---
+++ resources/views/admin/peminjaman.blade.php
@@ -0,0 +1,161 @@
+@extends('layout.layout')
+@section('content')
+@include('sweetalert::alert')
+
+{{-- cdn toastr --}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+<div class="content-body">
+
+    <div class="row page-titles mx-0">
+        <div class="col p-md-0">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="javascript:void(0)">Dashboard</a></li>
+                <li class="breadcrumb-item active"><a href="javascript:void(0)">Home</a></li>
+            </ol>
+        </div>
+    </div>
+    <!-- row -->
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Data Peminjaman</h4>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalCreate"><i class="fa fa-plus"></i> Tambah Data</button>
+                        <div class="table-responsive">
+                            <table class="table table-striped table-bordered zero-configuration">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Nama</th>
+                                        <th>Judul Buku</th>
+                                        <th>Tanggal Pinjam</th>
+                                        <th>Tanggal Kembali</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php
+                                        $no = 1;
+                                    @endphp
+                                    @foreach ($peminjaman as $item)
+                                    <tr>
+                                        <td>{{$no++}}</td>
+                                        <td>{{$item->nama}}</td>
+                                        <td>{{$item->judul_buku}}</td>
+                                        <td>{{$item->tgl_peminjaman}}</td>
+                                        <td>{{$item->tgl_kembali}}</td>
+                                        <td>
+
+                                            <a href="#" data-target="#modalEdit{{ $item->id }}" data-toggle="modal" class="btn btn-primary"><i class="fa fa-edit"></i></a>
+
+                                            <a href="{{ asset('admin/delete-peminjaman/' .$item->id)}}" button type="button" class="btn btn-danger" onclick="return confirm('Yakin Akan Menghapus Data Ini!?')"></button><i class="fa fa-trash"></i></a>
+
+                                        </td>
+                                    </tr>
+                                </tbody>
+                                @endforeach
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- #/ container -->
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="modalCreate">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Tambah Data Peminjam</h5>
+                <button type="button" class="close" data-dismiss="modal"><span>&times;</span>
+                </button>
+            </div>
+            <form action="create-peminjaman" method="POST">
+                @csrf
+            <div class="modal-body">
+                <label>Nama</label>
+                <div class="form-groub">
+                    <input type="text" class="form-control mb-4" name="nama" placeholder="Masukkan nama">
+                </div>
+                <label>Judul Buku</label>
+                <div class="form-groub">
+                    <input type="text" class="form-control mb-4" name="judul_buku" placeholder="Masukkan judul buku">
+                </div>
+                <label>Tanggal Peminjaman</label>
+                <div class="form-groub">
+                    <input type="date" class="form-control mb-4" name="tgl_peminjaman">
+                </div>
+                <label>Tanggal Kembali</label>
+                <div class="form-groub">
+                    <input type="date" class="form-control mb-4" name="tgl_kembali">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Simpan</button>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<!-- Modal EDIT -->
+@foreach ($peminjaman as $item)
+
+<div class="modal fade" id="modalEdit{{$item->id}}">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal"><span>&times;</span>
+                </button>
+            </div>
+            <form action="edit-peminjaman" method="POST">
+            @csrf
+            <div class="modal-body">
+                <input type="hidden" class="form-control" id="id" name="id" value="{{$item->id}}">
+                <label>Nama</label>
+                <div class="form-groub">
+                    <input type="text" class="form-control" name="nama" value="{{$item->nama}}" placeholder="masukkan nama">
+                </div>
+                <label>Judul Buku</label>
+                <div class="form-groub">
+                    <input type="text" class="form-control" name="judul_buku" value="{{$item->judul_buku}}"  placeholder="masukkan judul buku">
+                </div>
+                <label>Tanggal Peminjaman</label>
+                <div class="form-groub">
+                    <input type="date" class="form-control" name="tgl_peminjaman" value="{{$item->tgl_peminjaman}}"  placeholder="masukkan tanggal peminjaman">
+                </div>
+                <label>Tanggal Kembali</label>
+                <div class="form-groub">
+                    <input type="date" class="form-control" name="tgl_kembali" value="{{$item->tgl_kembali}}"  placeholder="masukkan tanggal kembali">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Save changes</button>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+@endforeach
+
+{{-- cdn toastr --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script>
+@if (Session::has('success'))
+    toastr.success("{{ Session::get('success')}}")
+@endif
+</script>
+@endsection
